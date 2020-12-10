0ad209ceda8dfc8306a6fa5787f29efcfac85487
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-12-10 06:17:03 +0530

    Added featured section

--- src/Home.js
+++ src/Home.js
@@ -44,7 +44,7 @@ export default function Home() {
                     {CardsData.map(card)}
                 </div>
             </div>
-
+            
             <FeaturedDishes />
         </>
     )
--- src/pages/home/data/FeaturedDishesDetail.js
+++ src/pages/home/data/FeaturedDishesDetail.js
@@ -1,36 +1,55 @@
 import img1 from "../../../assets/a1.png"
+import img2 from "../../../assets/a2.png"
+import img3 from "../../../assets/g5.jpg"
+import img4 from "../../../assets/g6.jpg"
+import img5 from "../../../assets/g8.jpg"
+import img6 from "../../../assets/roll.jpg"
 
+
+//price of dish
+const rate1 = "$18.5"
+const rate2 = "$14.5"
+const rate3 = "$13.5"
+const rate4 = "$12.5"
+const rate5 = "$15.5"
+const rate6 = "$17.5"
 
 const FeaturedDishesDetail = [
     {   
         dishImg: img1,
         dishName: 'Vegetarian',
+        price: rate1,
         desc: "Chicken, Mushroom, Onion, Bacon, BBQ sauce & Caeser sauce"
     },
     {   
-        dishImg: img1,
-        dishName: 'Vegetarian',
-        desc: "Chicken, Mushroom, Onion, Bacon, BBQ sauce & Caeser sauce"
+        dishImg: img2,
+        dishName: 'Ravioli',
+        price: rate2,
+        desc: "Meat / Ricotta & Spinach (Can be served with all sauces)"
     },
     {   
-        dishImg: img1,
-        dishName: 'Vegetarian',
-        desc: "Chicken, Mushroom, Onion, Bacon, BBQ sauce & Caeser sauce"
+        dishImg: img3,
+        dishName: 'Lasagne',
+        price: rate3,
+        desc: "Layers of pasta with bolognaise sauce, egg, ham, & cheese"
     },
     {   
-        dishImg: img1,
-        dishName: 'Vegetarian',
-        desc: "Chicken, Mushroom, Onion, Bacon, BBQ sauce & Caeser sauce"
+        dishImg: img4,
+        dishName: 'Pasta Feast Deal',
+        price: rate4,
+        desc: "Filled with Spinach & Ricotta in Napoletana sauce"
     },
     {   
-        dishImg: img1,
-        dishName: 'Vegetarian',
-        desc: "Chicken, Mushroom, Onion, Bacon, BBQ sauce & Caeser sauce"
+        dishImg: img5,
+        dishName: 'Canelloni',
+        price: rate5,
+        desc: "Take away only Choice of pasta & one sauce served in one container"
     },
     {   
-        dishImg: img1,
-        dishName: 'Vegetarian',
-        desc: "Chicken, Mushroom, Onion, Bacon, BBQ sauce & Caeser sauce"
+        dishImg: img6,
+        dishName: 'Chicken Caesar',
+        price: rate6,
+        desc: "Cheese, Olives, Capsicum & Mushroom"
     },
     
 ]
