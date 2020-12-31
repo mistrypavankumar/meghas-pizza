9e1fb6f6c85e9049384b23237afc7f0471c690a1
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-12-31 22:01:41 +0530

    Added mobile view slidemenu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import Button from './Button';
 
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import PhoneOutlinedIcon from '@material-ui/icons/PhoneOutlined';
+import MenuIcon from '@material-ui/icons/Menu';
 // import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 // import AccessTimeOutlinedIcon from '@material-ui/icons/AccessTimeOutlined';
 
@@ -17,6 +18,7 @@ export default function Navbar() {
     const total_amount = 0.00;
     
     const [navbar, setNavbar] = useState(false);
+    const [openSlide, setOpenSlide] = useState(false);
 
 
     const iconstyle = {
@@ -42,12 +44,12 @@ export default function Navbar() {
     window.addEventListener('scroll', changeBg);
 
    
+    const openSlide_ = () =>setOpenSlide(true);
+    const closeSlide_ = () => setOpenSlide(false);
+
     return (
         <>
-            {/*Mobile view logo  */}
-            <div className= "logo">
-
-            </div>
+            
             <nav class={navbar ? "main_navbar nav_active" : "main_navbar"}>
                 <div className={navbar ? "navbar nav_active" : "navbar"} >
                     <ul>
@@ -76,6 +78,47 @@ export default function Navbar() {
                             width="120px"
                             height="7vh"
                             custom_btn_style={navbar ? "cbtnStyle nav_active" : "cbtnStyle"}
+                        />
+                    </div>
+                </div>
+            </nav>
+
+            {/*Mobile view logo  */}
+             <div className= "logo"></div>
+             <div className="mobileNavBarContainer">
+                 <div class = {openSlide ? "menubarIcon active":"menubarIcon"} onClick = {openSlide_}>
+                    <MenuIcon style = {{fontSize: "40px"}}/>
+                </div>
+                <div className = "menubarIcon1">
+                    <ShoppingCartOutlinedIcon style = {{fontSize: "40px"}}/>
+                </div>
+             </div>
+            <nav className = {openSlide ? "mobile_nav active":"mobile_nav"} onClick = {closeSlide_}>
+                
+                <div className= {openSlide ?  "sliderContainner active" : "sliderContainner"}>
+                    <div className="sliderContent">
+                        <ul>
+                            <li><a href="#home">Home</a> </li>
+                            <li><a href="#home">Menu</a> </li>
+                            <li><a href="#home">Features</a> </li>
+                            <li><a href="#home">News</a> </li>
+                            <li><a href="#home">Reservation</a> </li>
+                            <li><a href="#home">Contacs</a> </li>
+                            <hr style = {{
+                                width: "100%",
+                                backgroundColor: 'rgb(158, 5, 5)'
+                            }} />
+                            <h3><PhoneOutlinedIcon />  123-456-7890</h3> 
+
+                        </ul>
+
+                        <Button
+                            buttonName="BOOK A TABLE "
+                            borderStyle="none"
+                            borderRadius="50px"
+                            width="120px"
+                            height="7vh"
+                            custom_btn_style="mobileNavBtn"
                         />
                     </div>
                 </div>
